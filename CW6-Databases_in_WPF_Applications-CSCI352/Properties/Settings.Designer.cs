3b171999489b069f3efcb4b4dd8ae8d843b9cdb6
Zachary Rose <zacerose@ut.utm.edu>
2023-02-09 14:10:08 -0600

    Created and linked Database to project. Haven't started the window yet.

--- CW6-Databases_in_WPF_Applications-CSCI352/Properties/Settings.Designer.cs
+++ CW6-Databases_in_WPF_Applications-CSCI352/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CW6_Databases_in_WPF_Applications_CSCI352.Properties
-{
-
-
+namespace CW6_Databases_in_WPF_Applications_CSCI352.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\CW6-Databases_in_WP" +
+            "F_Applications.accdb")]
+        public string CW6_Databases_in_WPF_ApplicationsConnectionString {
+            get {
+                return ((string)(this["CW6_Databases_in_WPF_ApplicationsConnectionString"]));
             }
         }
     }
