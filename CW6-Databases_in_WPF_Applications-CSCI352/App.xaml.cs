389b0cca80dadbf3ad0e829a01f0918d869b7bf4
Zachary Rose <zacerose@ut.utm.edu>
2023-02-09 14:05:16 -0600

    Add project files.

---
+++ CW6-Databases_in_WPF_Applications-CSCI352/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CW6_Databases_in_WPF_Applications_CSCI352
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
