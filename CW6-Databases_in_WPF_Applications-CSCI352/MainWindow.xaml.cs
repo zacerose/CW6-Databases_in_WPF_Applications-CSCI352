f3ebea9fbf817acb36e8f5007f55feb510562764
Zachary Rose <zacerose@ut.utm.edu>
2023-02-10 11:40:15 -0600

    Added basic interface and See Assets basic logic. Still seems to need a driver installed.

--- CW6-Databases_in_WPF_Applications-CSCI352/MainWindow.xaml.cs
+++ CW6-Databases_in_WPF_Applications-CSCI352/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +22,27 @@ namespace CW6_Databases_in_WPF_Applications_CSCI352
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
         public MainWindow()
         {
             InitializeComponent();
+            cn = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source =| DataDirectory |\\CW6 - Databases_in_WPF_Applications.accdb");
+        }
+
+        private void btn_see_assets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+           
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while(read.Read())
+            {
+                data += read[0].ToString() + "\n";
+            }
+            txt_display.Text = data;
         }
     }
 }
